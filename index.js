75f26e69ca866f458c43db893672fb52e9823e8b
Espebob <mespe05@gmail.com>
2024-05-23 13:59:13 +0100

    adding cors

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ import errorHandler from './middleware/errorhandler.js';
 import dotenv from 'dotenv';
 import documentation from "./doc/swagger.json" assert{type:"json"};
 import swaggerUi from "swagger-ui-express";
+import cors from "cors"
 
+const corsOptions ={
+    allowedHeaders: ["Authorization", "Content-Type" ],
+    methods: ["GET", "POST", "PUT", "UPDATE", "DELETE"],
+    origin:"*",
+};
 
 dotenv.config()
 
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api_docs",swaggerUi.serve, swaggerUi.setup(documentation))
 app.use('/parent',router);
